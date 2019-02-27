0fe9f382c9f620141cee2bb933f35ee99a41a6dd
mdemouch@gmail.com <mdemouch@gmail.com>
2019-02-27 15:13:05 +0100

    test cuda

--- pysc2_codelab_material/install-cuda.sh
+++ pysc2_codelab_material/install-cuda.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 #Instalamos Cuda
-#sudo apt-get install nvidia-cuda-toolkit
-
-#wget https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda_10.0.130_410.48_linux
-#chmod 777 cuda_10.0.130_410.48_linux
-#sudo ./cuda_10.0.130_410.48_linux
-#rm cuda_10.0.130_410.48_linux
+sudo dpkg -i cuda-repo-ubuntu1804-10-1-local-10.1.105-418.39_1.0-1_amd64.deb
+sudo apt-key add /var/cuda-repo-10-1-local-10.1.105-418.39/7fa2af80.pub
+sudo apt-get update
+sudo apt-get install cuda
--- pysc2_codelab_material/install-deps-1.sh
+++ pysc2_codelab_material/install-deps-1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install nvidia-cuda-toolkit
+#sudo apt-get install nvidia-cuda-toolkit
 sudo apt-get install python3 python3-dev python3-pip
 
-sudo nano /usr/lib/pip3
+#sudo nano /usr/lib/pip3
